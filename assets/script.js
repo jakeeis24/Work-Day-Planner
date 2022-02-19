e8e2e45f5d145c556fbbeedf954eb907661bee9b
Jake Eissler <jakeeissler1@gmail.com>
2022-02-19 11:54:51 -0700

    added the click function to save user input to local storage. The user will now be able to save and see their inputs.

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,17 @@ var militaryHour = moment().hour();
 //array for time blocks starting at 9 to run against military time
 var timeBlocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 //console.log(timeBlocks);
-
+var timeText = [
+  "9AM",
+  "10AM",
+  "11AM",
+  "12PM",
+  "1PM",
+  "2PM",
+  "3PM",
+  "4PM",
+  "5PM",
+];
 // ---WHEN I scroll down
 // ---THEN I am presented with timeblocks for standard business hours
 // ---WHEN I view the timeblocks for that day
@@ -34,6 +44,24 @@ for (i = 0; i < timeBlocks.length; i++) {
 // WHEN I click into a timeblock - textarea
 // THEN I can enter an event
 // WHEN I click the save button for that timeblock -event listener
+var saveBtn = $(".saveBtn");
+saveBtn.on("click", function () {
+  console.log($(this).siblings("textarea").val());
+  console.log($(this).siblings("div").text());
+  var textBox = $(this).siblings("textarea").val();
+  var timeData = $(this).siblings("div").text();
+  localStorage.setItem(timeData, textBox);
+  populateData();
+});
 // THEN the text for that event is saved in local storage localStorage.setItem
 // WHEN I refresh the page
 // THEN the saved events persist
+function populateData() {
+  for (let i = 0; i < timeText.length; i++) {
+    var saveTime = localStorage.getItem(timeText[i]);
+    $("#hr" + i)
+      .children("textarea")
+      .text(saveTime);
+  }
+}
+populateData();
